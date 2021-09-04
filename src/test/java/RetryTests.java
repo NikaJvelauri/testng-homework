adf24a73260b87d8d468e6f1d2ff50ecd0f332e1
NikaJvelauri <72291952+NikaJvelauri@users.noreply.github.com>
2021-09-04 11:17:04 +0400

    Update RetryTests.java
    gvian gavige am 10-is shesaxeb chatshi gaurkvevloba iyo da dilitga daiwera ra igulisxmeboda 10-shi :D

--- src/test/java/RetryTests.java
+++ src/test/java/RetryTests.java
@@ -4,7 +4,7 @@ import org.testng.ITestResult;
 //Write your own retry annotation
 public class RetryTests implements IRetryAnalyzer {
         private int retryNumber = 0;
-        private static int maxRetries = 5;
+        private static int maxRetries = 10;
         @Override
         public boolean retry(ITestResult result) {
             if (retryNumber < maxRetries) {
